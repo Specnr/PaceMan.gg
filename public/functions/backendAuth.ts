15875ea0aa2d27efd5836329db5dc6df1cef668f
Specnr <specnrr@gmail.com>
2023-08-25 17:03:54 -0400

    remove accident lol

--- public/functions/backendAuth.ts
+++ public/functions/backendAuth.ts
@@ -13,32 +13,3 @@ export const isValidAuth = (req: NextApiRequest, res: NextApiResponse) => {
   }
   return true;
 }
-
-const sendCode = (code: any) => {
-  const formData = new URLSearchParams({
-    client_id: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID || "",
-    client_secret: process.env.TWITCH_SECRET || "",
-    code: code,
-    grant_type: 'authorization_code',
-    redirect_uri: process.env.TOKEN_REDIRECT_URL || ""
-  });
-
-  try {
-    return axios.post("https://id.twitch.tv/oauth2/token", formData.toString(), {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-  } catch (error: any) {
-    console.log(error.message);
-  }
-}
-
-const getUser = (token: any) => {
-  return axios.get("https://api.twitch.tv/helix/users", {
-    headers: {
-      Authorization: "Bearer " + token,
-      "Client-Id": process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID
-    }
-  })
-}
