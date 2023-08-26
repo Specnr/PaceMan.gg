20545b4192c651ecb49d322719798c6dc733155f
Specnr <specnrr@gmail.com>
2023-08-26 14:17:09 -0400

    removed force verify

--- components/twitch/Twitch-OAuth.ts
+++ components/twitch/Twitch-OAuth.ts
@@ -4,7 +4,7 @@ export const getTwitchOAuthURL = (mcToken: string) => {
     redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URL || "",
     response_type: "code",
     scope: "openid",
-    force_verify: "true",
+    force_verify: "false",
     state: mcToken
   });
   return new URL(`https://id.twitch.tv/oauth2/authorize?${decodeURIComponent(params.toString())}`);
