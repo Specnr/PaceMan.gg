07fdfb5f0e3b8e1ce07242a2b154d7bf16349b93
Specnr <specnrr@gmail.com>
2023-11-23 18:32:03 -0500

    fix link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
             Generate Access Token
           </button>
         </div>
-        <Link link="https://docs.google.com/document/d/118WJx5C9giWHsdc-CShhoF_6yKrPsTFB_edGmtR9F-ks">Rules</Link>{" • "}
+        <Link link="https://docs.google.com/document/d/118WJx5C9giWHsdc-CShhoF_6yKrPsTFB_edGmtR9F-k">Rules</Link>{" • "}
         <Link link="https://github.com/PaceMan-MCSR/PaceMan-Tracker/releases">Tracker</Link>{" • "}
         <Link link="https://docs.google.com/document/d/1RgHUJhLvnUp0KtnCcdJEHCi6rn7naUqWwfe-0ntTAlo">Tutorial</Link>{" • "}
         <Link link="https://discord.gg/t63gGSWvdV">Help</Link>{" • "}
