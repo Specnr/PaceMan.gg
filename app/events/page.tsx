fd3e54a74f4423375d890e57694da876d0afde24
Specnr <specnrr@gmail.com>
2023-11-27 11:48:48 -0500

    improve loading looks

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -27,37 +27,40 @@ export default function Events() {
   let msg = null;
   if (error || (!isLoading && !events)) msg = "failed to load";
   if (isLoading || !searchParams || !events) msg = <Loading />;
-  if (searchParams && !selectedEvent && searchParams.get("eventId") !== null) msg = "invalid event id";
+  if (!isLoading && searchParams && !selectedEvent && searchParams.get("eventId") !== null) msg = "invalid event id";
 
   const eventList = events ? events : [];
   return (
-    <div className="container-height">
-      <div className="pt-16">
-        <Title titleOverrite={selectedEvent ? selectedEvent.name : undefined} />
-        {selectedEvent && (
-          <p className="pb-2">
-            {msToDate(selectedEvent.starts[0])} - {msToDate(selectedEvent.ends[selectedEvent.ends.length-1])}
-          </p>
-        )}
-        <select
-          onChange={evt => setSelectedEvent(eventList.filter((e) => e._id === evt.target.value)[0])}
-          className="
-            md:w-80
-            mx-auto
-            bg-gray-50 border border-gray-300
-            text-gray-900 text-sm rounded-lg
-            block w-full p-2.5 dark:bg-gray-700
-            dark:border-gray-600 dark:placeholder-gray-400
-            dark:text-white">
-              <option className="font-sans">Choose an event</option>
-          {
-            eventList.map(e => (<option className="font-sans" value={e._id} key={e._id}>{e.name}</option>))
-          }
-        </select>
+    msg !== null ? 
+    <div className="container-height grid place-items-center">{msg}</div>: (
+      <div className="container-height">
+        <div className="pt-16">
+          <Title titleOverrite={selectedEvent ? selectedEvent.name : undefined} />
+          {selectedEvent && (
+            <p className="pb-2">
+              {msToDate(selectedEvent.starts[0])} - {msToDate(selectedEvent.ends[selectedEvent.ends.length-1])}
+            </p>
+          )}
+          <select
+            onChange={evt => setSelectedEvent(eventList.filter((e) => e._id === evt.target.value)[0])}
+            className="
+              md:w-80
+              mx-auto
+              bg-gray-50 border border-gray-300
+              text-gray-900 text-sm rounded-lg
+              block w-full p-2.5 dark:bg-gray-700
+              dark:border-gray-600 dark:placeholder-gray-400
+              dark:text-white">
+                <option className="font-sans">Choose an event</option>
+            {
+              eventList.map(e => (<option className="font-sans" value={e._id} key={e._id}>{e.name}</option>))
+            }
+          </select>
+        </div>
+        {
+          !selectedEvent ? <div className="grid h-4/6 place-items-center"></div> : <EventTable event={selectedEvent} />
+        }
       </div>
-      {
-        !selectedEvent ? <div className="grid h-4/6 place-items-center"></div> : <EventTable event={selectedEvent} />
-      }
-    </div>
+    )
   );
 }
