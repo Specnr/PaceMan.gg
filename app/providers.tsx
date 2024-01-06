07369a5288d7d9ff4b4d89d0642eda8f61e7b10c
Specnr <specnrr@gmail.com>
2024-01-06 16:19:08 -0500

    add route loading bar

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,19 @@
+"use client";
 import * as React from "react";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { Next13ProgressBar } from "next13-progressbar";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <>
+      <NextUIProvider>{children}</NextUIProvider>
+      <Next13ProgressBar
+        height="4px"
+        color="rgb(147 51 234 / var(--tw-bg-opacity))"
+        options={{ showSpinner: false }}
+        showOnShallow
+      />
+    </>
+  );
 }
