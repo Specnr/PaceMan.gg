eab4ee6f9a7588b6deebf36514f477cf7eeb7dbf
Specnr <specnrr@gmail.com>
2024-10-19 12:07:32 -0400

    liveruns filter w localstorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+"use client"
 import Title from "@/components/Title";
 import { Tooltip } from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo, faSliders } from "@fortawesome/free-solid-svg-icons";
 import PaceLeaderboard from "@/components/Leaderboards/PaceLeaderboard";
+import { useEffect, useState } from "react";
+import SettingsTable from "@/components/SettingsTable";
+import { PaceSettings } from "@/components/interfaces/Pace";
 
 export default function Home() {
+  const [showSettings, setShowSettings] = useState(false)
+  const [settings, setSettings] = useState<PaceSettings>({ version: "1.16.1", liveOnly: false });
+
+  useEffect(() => {
+    const storedSettings = localStorage.getItem("PaceMan-Pace-Settings")
+    if (storedSettings) {
+      setSettings(JSON.parse(storedSettings))
+    }
+  }, []);
+
+  console.log(settings)
+
   return (
     <div className="container-height">
       <div className="pt-16">
@@ -26,9 +42,11 @@ export default function Home() {
           >
             <FontAwesomeIcon icon={faCircleInfo} className="pl-2" />
           </Tooltip>
+          <FontAwesomeIcon icon={faSliders} className="pl-2" onClick={() => setShowSettings(!showSettings)} />
         </p>
       </div>
-      <PaceLeaderboard />
+      {showSettings && <SettingsTable settings={settings} setSettings={setSettings} />}
+      <PaceLeaderboard settings={settings} />
     </div>
   );
 }
