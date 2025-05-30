5f44e1e90d94cd2f33f67f9c0120e20d16e82db1
Specnr <specnrr@gmail.com>
2025-05-30 17:25:07 -0400

    add player count

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,32 @@
 import Title from "@/components/Title";
 import { Tooltip } from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo, faSliders } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo, faSliders, faUser } from "@fortawesome/free-solid-svg-icons";
 import PaceLeaderboard from "@/components/Leaderboards/PaceLeaderboard";
 import { useEffect, useState } from "react";
 import SettingsTable from "@/components/SettingsTable";
 import { PaceSettings } from "@/components/interfaces/Pace";
+import useSWR from "swr";
+
+const playerFetcher = async (url: string) => {
+  const data = await fetch(url).then((res) => res.json());
+  return data.playerList;
+};
 
 export default function Home() {
   const [showSettings, setShowSettings] = useState(false)
   const [settings, setSettings] = useState<PaceSettings>({ version: "1.16.1", liveOnly: false });
+  
+  const { data: playerList } = useSWR(
+    "https://paceman.gg/api/ars/players",
+    playerFetcher,
+    {
+      refreshWhenHidden: true,
+      refreshInterval: 1000,
+    }
+  );
+  
+  const playerCount = playerList?.length || 0;
 
   useEffect(() => {
     const storedSettings = localStorage.getItem("PaceMan-Pace-Settings")
@@ -18,8 +35,6 @@ export default function Home() {
       setSettings(JSON.parse(storedSettings))
     }
   }, []);
-
-  console.log(settings)
 
   return (
     <div className="container-height">
@@ -43,6 +58,10 @@ export default function Home() {
             <FontAwesomeIcon icon={faCircleInfo} className="pl-2" />
           </Tooltip>
           <FontAwesomeIcon icon={faSliders} className="pl-2" onClick={() => setShowSettings(!showSettings)} />
+          <span className="pl-4">
+            <FontAwesomeIcon icon={faUser} className="pr-1" />
+            {playerCount}
+          </span>
         </p>
       </div>
       {showSettings && <SettingsTable settings={settings} setSettings={setSettings} />}
